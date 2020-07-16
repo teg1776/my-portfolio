ff16f7b0b7a060cc48caf670763f293d58ecc25c
teg1776 <teg1776@gmail.com>
2020-07-15 22:13:29 -0500

    Updates

---
+++ precache-manifest.37c6c539d01b79163d1c0410f2d49336.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4844c73fb66cbfb021f69e9444695416",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "13eae21a6cb7e6e983c9",
+    "url": "/my-portfolio/static/css/main.0f4701a1.chunk.css"
+  },
+  {
+    "revision": "cb130a62b09e92273334",
+    "url": "/my-portfolio/static/js/2.1ec63d56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.1ec63d56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13eae21a6cb7e6e983c9",
+    "url": "/my-portfolio/static/js/main.0c55b31c.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "6e5b96aedcb77318fea0e389c1799446",
+    "url": "/my-portfolio/static/media/git.6e5b96ae.png"
+  },
+  {
+    "revision": "d81d1873cf8092e6c31526f1af299dcd",
+    "url": "/my-portfolio/static/media/grill-time-screenshot.d81d1873.png"
+  },
+  {
+    "revision": "ab1ec42eb0b0ead02ced3791e12fa0b7",
+    "url": "/my-portfolio/static/media/insta.ab1ec42e.png"
+  },
+  {
+    "revision": "fc9ef9d493c7e7836dcac572dd605099",
+    "url": "/my-portfolio/static/media/linkedin.fc9ef9d4.png"
+  },
+  {
+    "revision": "bc58887af31bba8f1612bf867a960a1f",
+    "url": "/my-portfolio/static/media/logoS.bc58887a.png"
+  },
+  {
+    "revision": "12774433b32462773896c7ea2046b379",
+    "url": "/my-portfolio/static/media/ocean.12774433.jpg"
+  },
+  {
+    "revision": "1607a3a71b55547fb4da1ce188c1bc1b",
+    "url": "/my-portfolio/static/media/source-code.1607a3a7.png"
+  },
+  {
+    "revision": "d3e638c9435f4aa052245478da37c3cf",
+    "url": "/my-portfolio/static/media/tie.d3e638c9.jpeg"
+  },
+  {
+    "revision": "e0421520506e4ff8dc158d95902cd25b",
+    "url": "/my-portfolio/static/media/work-link.e0421520.png"
+  },
+  {
+    "revision": "fe7c712ee4e5013431421f3787d94864",
+    "url": "/my-portfolio/static/media/youtube.fe7c712e.png"
+  }
+]);
